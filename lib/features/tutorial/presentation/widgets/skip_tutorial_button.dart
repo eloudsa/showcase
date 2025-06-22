f26c4915f4b29b3b369ffee2762f319fe0739649
Said <300818+eloudsa@users.noreply.github.com>
2025-06-22 13:20:19 +0200

    Exceptions fixed

--- lib/features/tutorial/presentation/widgets/skip_tutorial_button.dart
+++ lib/features/tutorial/presentation/widgets/skip_tutorial_button.dart
@@ -1,18 +1,14 @@
 import 'package:flutter/material.dart';
 
 Widget buildSkipTutorialButton({required VoidCallback onSkip}) {
-  return Positioned(
-    top: 40.0,
-    right: 20.0,
-    child: TextButton(
-      onPressed: onSkip,
-      child: const Text(
-        "SKIP",
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 16.0,
-          fontWeight: FontWeight.bold,
-        ),
+  return TextButton(
+    onPressed: onSkip,
+    child: const Text(
+      "SKIP",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 16.0,
+        fontWeight: FontWeight.bold,
       ),
     ),
   );
